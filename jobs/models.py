4e08a42a7b82b46f066ec961c9450d224e9168b3
dannymevs@hotmail.com <dannymevs@hotmail.com>
2022-12-07 15:36:27 -0500

    fixed issue in home page

--- jobs/models.py
+++ jobs/models.py
@@ -1,9 +1,22 @@
 from django.db import models
+# from django.contrib import admin
 
-# Create your models here.
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    github_link = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.summary
+
+# Create your models here.
+# @admin.register(models.Model)
+# class Job(admin.ModelAdmin):
+#     image = models.ImageField(upload_to='images/')
+#     summary = models.CharField(max_length=200)
+#     github_link = models.CharField(max_length=200)
+#
+#     def __str__(self):
+#         return self.summary
+
+
